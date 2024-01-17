61406554c4364c1017401fe238ff738abf8d7648
Crowdbotics <team@crowdbotics.com>
2024-01-17 21:25:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fun-box-45417",
+  url: "https://fun-box-45417.botics.co",
+  api: "https://fun-box-45417.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
